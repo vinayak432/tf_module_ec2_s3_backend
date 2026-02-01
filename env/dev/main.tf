ca561689f8fb13736e55fa77080ed20fa885b5be
vinayak432 <53861374+vinayak432@users.noreply.github.com>
2026-02-01 15:43:52 +0530

    Update AMI ID for EC2 module in main.tf

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -1,7 +1,7 @@
-module "network_ec2" {
+module "ec2_vpc" {
   source         = "../modules/ec2_vpc"
   vpc_cidr       = "10.0.0.0/16"
   subnet_cidr    = "10.0.1.0/24"
-  ami_id         = "ami-id"
+  ami_id         = "ami-04b0f5834ea4c3e32"
   instance_type  = "t3.micro"
 }
